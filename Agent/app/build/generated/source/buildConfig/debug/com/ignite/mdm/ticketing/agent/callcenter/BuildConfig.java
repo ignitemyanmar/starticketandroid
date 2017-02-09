d6ad289db2795e88489235c6e1979d320e0b2e06
Ye lin aung <hello.yelinaung@gmail.com>
2017-02-09 22:39:49 +0630

    Added Version COdes

--- Agent/app/build/generated/source/buildConfig/debug/com/ignite/mdm/ticketing/agent/callcenter/BuildConfig.java
+++ Agent/app/build/generated/source/buildConfig/debug/com/ignite/mdm/ticketing/agent/callcenter/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ignite.mdm.ticketing.agent.callcenter";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2;
-  public static final String VERSION_NAME = "@string/app_version";
+  public static final int VERSION_CODE = 11100;
+  public static final String VERSION_NAME = "11100";
 }
--- Agent/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ Agent/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="a171db71-991c-4b5d-a20a-79f593048a0b", modulePackage="com.ignite.mdm.ticketing.agent.callcenter", sdkRoot="/Users/user/Library/Android/sdk",layoutInfoDir="/Users/user/Desktop/WorkProjects/starticketandroid/Agent/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=16,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="5d854222-0699-4c1a-8c20-a91f7f7ff6d9", modulePackage="com.ignite.mdm.ticketing.agent.callcenter", sdkRoot="/Users/user/Library/Android/sdk",layoutInfoDir="/Users/user/Desktop/WorkProjects/starticketandroid/Agent/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=16,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
